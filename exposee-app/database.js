58adc346b387809c93a393c41df00e718ca568cc
Godfred Asamoah <realswat@meta.com>
2023-07-09 14:54:57 -0700

    worked on server and db

---
+++ exposee-app/database.js
@@ -0,0 +1,9 @@
+<<<<<<< Updated upstream
+=======
+import { Sequelize } from 'sequelize';
+
+export const sequelize = new Sequelize('exposeedb', 'exposeedbuser', 'iamtheSwat1+', {
+  host: 'localhost',
+  dialect: 'postgres'
+});
+>>>>>>> Stashed changes
