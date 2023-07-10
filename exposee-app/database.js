97ebd220ccd6cad6bd8d3100b36692f37ef0f90b
realswat1 <136376702+realswat1@users.noreply.github.com>
2023-07-09 21:27:11 -0700

    Update database.js

--- exposee-app/database.js
+++ exposee-app/database.js
@@ -1,18 +1,8 @@
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-=======
+
 import { Sequelize } from 'sequelize';
 
 export const sequelize = new Sequelize('exposeedb', 'exposeedbuser', 'iamtheSwat1+', {
   host: 'localhost',
   dialect: 'postgres'
 });
->>>>>>> Stashed changes
-=======
-import { Sequelize } from 'sequelize';
 
-export const sequelize = new Sequelize('capstone', 'dbuser', 'password', {
-  host: 'localhost',
-  dialect: 'postgres'
-});
->>>>>>> main
