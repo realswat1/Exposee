7a7ffd2069335cbab89c0b40657b448f2da7dfc1
realswat1 <136376702+realswat1@users.noreply.github.com>
2023-07-11 22:44:55 -0700

    Update Registration.jsx

--- exposee-ui/src/components/Registration/Registration.jsx
+++ exposee-ui/src/components/Registration/Registration.jsx
@@ -1,26 +1,15 @@
 import React, { useState, useContext } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-<<<<<<< HEAD
 import '../Registration/Registration.css'
 import { UserContext } from '../../UserContext';
 
-=======
-
-import '../Registration/Registration.css'
-import { UserContext } from '../../UserContext';
-
-
->>>>>>> 5ff522a4274442e80a92082320eae2e03e9395d9
 const Registration = () => {
   const [username, setUsername] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
 
-<<<<<<< HEAD
-=======
 
->>>>>>> 5ff522a4274442e80a92082320eae2e03e9395d9
   // const updateUser  = 
   // useContext(UserContext);
   //const navigate = useNavigate();
@@ -30,30 +19,19 @@ const Registration = () => {
 
     try {
       // Make the signup API request
-<<<<<<< HEAD
-      const response = await fetch(`http://localhost:3000/user`, {
-=======
 
       const response = await fetch(`http://localhost:3000/user`, {
 
->>>>>>> 5ff522a4274442e80a92082320eae2e03e9395d9
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
-<<<<<<< HEAD
-=======
 
->>>>>>> 5ff522a4274442e80a92082320eae2e03e9395d9
         body: JSON.stringify({ username: username, email: email, password: password}),
         credentials: 'include'
         
       });
-<<<<<<< HEAD
-console.log("HWY",response);
-=======
 
->>>>>>> 5ff522a4274442e80a92082320eae2e03e9395d9
       if (response.ok) {
         const data = await response.json();
         const loggedInUser = data.user;
@@ -66,15 +44,9 @@ console.log("HWY",response);
         setPassword('');
 
         // Update the user context
-<<<<<<< HEAD
-        useContext(loggedInUser);
-
-=======
 
         useContext(loggedInUser);
 
-
->>>>>>> 5ff522a4274442e80a92082320eae2e03e9395d9
         // Navigate to the home page after successful login
         navigate('/');
       } else {
@@ -102,10 +74,6 @@ console.log("HWY",response);
             required
           />
         </div>
-<<<<<<< HEAD
-=======
-
->>>>>>> 5ff522a4274442e80a92082320eae2e03e9395d9
          <div className="form-group"> 
           <label htmlFor="email">Email:</label>
           <input
