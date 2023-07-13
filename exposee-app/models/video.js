6fc1c81687210c9007f10e0c2995ef2d0d513781
Godfred Asamoah <realswat@meta.com>
2023-07-12 18:41:43 -0700

    updated video model

--- exposee-app/models/video.js
+++ exposee-app/models/video.js
@@ -1,8 +1,41 @@
-import { DataTypes } from 'sequelize';
+import { DATE, DataTypes } from 'sequelize';
 import { sequelize } from '../database.js';
 
 export const Video = sequelize.define('Video', {
   title: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  description: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  duration: { 
+    type: DataTypes.DATE,
+    allowNull:false
+
+  },
+  user_id:{
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  created_at: {
+    type: DataTypes.TIME,
+    allowNull: false 
+  },
+  is_live: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false,
+  },
+  is_saved:{
+    type: DataTypes.BOOLEAN,
+    allowNull: false
+  },
+  updated_at:{
+    type: DataTypes.DATE,
+    allowNull: false
+  },
+  API_key:{
     type: DataTypes.STRING,
     allowNull: false
   },
