f64896d20bbafac6f77888df94886158e370e78d
Godfred Asamoah <realswat@meta.com>
2023-07-13 11:05:17 -0700

    got to identify error

--- exposee-ui/src/components/Mainpage/Main.jsx
+++ exposee-ui/src/components/Mainpage/Main.jsx
@@ -1,10 +1,10 @@
 import "./main.css";
 import { useState, useEffect, useContext } from "react";
-import { UserContext } from "../../UserContext.js";
-// import { Link } from "react-router-dom";
+import { UserContext } from "../../UserContext"; 
+import { Link } from "react-router-dom";
 
 function Main() {
-  const { user, updateUser } = useContext(UserContext);
+  // const {User, updateUser} = useContext(UserContext);
   const [videos, setVideos] = useState([]);
   const [currentIndex, setCurrentIndex] = useState(0);
 
@@ -45,17 +45,17 @@ function Main() {
   return (
     <div className="main">
       <header className="header">
-        <div className="user-info">
-          {user ? (
+        {/* <div className="user-info"> 
+           {User ? ( 
             <>
-              <span>Hi {user.username}! |</span>
-              {/* <Link to= "/profile"> Go to profile Page</Link> */}
+              <span>Hi {User.username}! |</span>
+               <Link to= "/profile"> Go to profile Page</Link> 
               <button onClick={handleLogout}>Logout</button>
             </>
           ) : (
             <Link to="/login">Login</Link>
           )}
-        </div>
+          </div>*/}
       </header>
       <div
         className="video-list"
