dde15029eca2b718aa94069c25152e4432cdb6ae
Godfred Asamoah <realswat@meta.com>
2023-07-14 13:05:41 -0700

    backend of profile page

--- exposee-app/server.js
+++ exposee-app/server.js
@@ -1,6 +1,8 @@
 // app.js
 import express from 'express';
 import session from 'express-session';
+import bodyParser from 'body-parser';
+import multer from 'multer';
 import cors from 'cors';
 import morgan from 'morgan';
 import { sequelize } from './database.js';
@@ -16,14 +18,26 @@ app.use(cors({
   origin: 'http://localhost:5173',
   credentials: true
 }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true }));
 app.use(express.json()); // Middleware for parsing JSON bodies from HTTP requests
 app.use(morgan())
+app.use(express.static('public'));
 
 const SequelizeStore = SequelizeStoreInit(session.Store);
 const sessionStore = new SequelizeStore({
   db: sequelize
 });
 
+const storage = multer.diskStorage({
+  destination: 'public/uploads',
+  filename: (req, file, cb) => {
+    const uniqueSuffix = Date.now() + '-'+ math.round(Math.random() * 1E9);
+    cb(null, file.filename + '_' + uniqueSuffix);
+  },
+});
+
+const upload= multer({storage});
 // Session middleware
 app.use(
   session({
@@ -42,6 +56,51 @@ app.use(
 sessionStore.sync();
 
 app.use(userRoutes);
+
+app.post('/upload-profile-pic', upload.single('profilePic'), (req, res)=>{
+  const filePath = file.filePath;
+  const userId = req.params.userId;
+  userId.update({ profilePicture: filePath}, {where: { Id: userId}})
+  .then (()=>{
+    res.status(200).json({message: 'Profile picture upload successfulloy'});
+  })
+  .catch(error=> {
+    console.log(error);
+    res.status(500).json({message: 'error uploading profile picture'});
+  });
+});
+app.get('/user/:userId', (req, res)=>{
+
+  const userId = req. params.userId;
+
+  userId.findOne ({where: { Id: userId}})
+  .then (user => {
+    if (user){
+      res.status(200).json(user);
+    } else {
+      res.status (404).json({message: 'user not found'});
+    } 
+  })
+  .catch(error =>{
+    console.log(error);
+    res.status(500).json({message: 'Error retrieveing user information'});
+  });
+});
+
+
+app.put('user/:userId', (req,res)=>{
+  const userId = req.params.userId;
+  const updatedInfo = req.body;
+
+  User.update( updatedInfo, { where: { id: userId}})
+  .then(()=>{
+    res.status(200).json({message: 'user information updated sucessfully '});
+  })
+  .catch(error => {
+    console.log(error);
+    res.status(500).json({message: 'error updating user information'});
+  });
+});
 
 
 app.get('/videos', async (req, res) => {
