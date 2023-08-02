a186bbbeb5b2559880af3bc1090e1357d735ae62
realswat1 <136376702+realswat1@users.noreply.github.com>
2023-08-01 18:23:17 -0700

    Loadingspinner (#29)
    * linter
    
    * fix one
    
    * loading spinner
    
    * css
    
    * final touches

---
+++ exposee-ui/src/components/LoadingSpinner/LoadingSpinner.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './LoadingSpinner.css';
+
+const LoadingSpinner = () => {
+    return (
+        <div className="loading-spinner">
+            <div className="loading-text">exposee</div>
+            <div className="dots-container">
+                <div className="dot"></div>
+                <div className="dot"></div>
+                <div className="dot"></div>
+            </div>
+        </div>
+    );
+};
+
+export default LoadingSpinner;
