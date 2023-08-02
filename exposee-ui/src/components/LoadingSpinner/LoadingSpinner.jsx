a0512883c60643d95e8e4c22b5532bf67b2f225e
Godfred Asamoah <realswat@meta.com>
2023-08-01 17:32:59 -0700

    final touches

--- exposee-ui/src/components/LoadingSpinner/LoadingSpinner.jsx
+++ exposee-ui/src/components/LoadingSpinner/LoadingSpinner.jsx
@@ -4,14 +4,14 @@ import './LoadingSpinner.css';
 const LoadingSpinner = () => {
     return (
         <div className="loading-spinner">
-          <div className="loading-text">exposee</div>
-          <div className="dots-container">
-            <div className="dot"></div>
-            <div className="dot"></div>
-            <div className="dot"></div>
-          </div>
+            <div className="loading-text">exposee</div>
+            <div className="dots-container">
+                <div className="dot"></div>
+                <div className="dot"></div>
+                <div className="dot"></div>
+            </div>
         </div>
-      );
-    };
+    );
+};
 
 export default LoadingSpinner;
